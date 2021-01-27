42588720960266a3f5ef544066707d22f2d8a27b
Uchinomaru <bravecred@gmail.com>
2021-01-27 12:56:03 +0900

    Add：会社詳細ページへのリンクを追加

--- src/Main/Company.js
+++ src/Main/Company.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CompanyList(props) {
   return(
     <tr>
-      <td>{ props.name }</td>
+      <td><Link to={"/company/show/" + props.id }>{ props.name }</Link></td>
       <td>{ props.progress }</td>
       <td>{ props.motivation }</td>
       <td>{ props.remarks }</td>
