f47f2a49a8d1efb88393c416e249d82d81b9ed20
Uchinomaru <bravecred@gmail.com>
2021-01-27 13:16:41 +0900

    Fix：ヘッダー内リンクを修正

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -39,12 +39,11 @@ export default function Header(props) {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">SMART就活</Navbar.Brand>
+        <Navbar.Brand href="/">SMART就活</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <LoggedInStatusDisplay />
           </Nav>
         </Navbar.Collapse>
